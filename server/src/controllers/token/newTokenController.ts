7c66f3396fa8f66023d8a212047590a7e90d0426
Çağan Seyrek <115536535+caganseyrek@users.noreply.github.com>
2024-08-06 20:12:32 +0300

    Completed token validation and new token creation

--- server/src/controllers/token/newTokenController.ts
+++ server/src/controllers/token/newTokenController.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import { sign, verify } from "jsonwebtoken";
 
 import tokenModel from "../../models/tokenModel";
 
 import {
   controllerError,
+  controllerMessages,
   statusMessages,
 } from "../../localization/messages_en";
 
@@ -17,10 +19,27 @@ const newTokenController = async (
   if (!currentUser) return res.status(400).send(statusMessages.badrequest);
 
   try {
-    const hasValidRefreshToken = await tokenModel
+    const hasRefreshToken = await tokenModel
       .findOne({ belongsTo: currentUser })
       .exec();
-    if (hasValidRefreshToken) return res.status(405).send();
+    if (!hasRefreshToken)
+      return res
+        .status(405)
+        .send(controllerMessages.token.newToken.noRefreshTokenFound);
+    const isRefreshTokenValid = verify(
+      hasRefreshToken.refreshToken as string,
+      process.env.JWT_REFRESH_SECRET! as string,
+    );
+    if (!isRefreshTokenValid)
+      return res
+        .status(405)
+        .send(controllerMessages.token.newToken.refreshTokenExpired);
+
+    const newAccessToken = sign(currentUser, process.env.JWT_ACCESS_SECRET!, {
+      expiresIn: "15m",
+    });
+
+    return res.status(201).send({ accessToken: newAccessToken });
   } catch (error) {
     console.error(controllerError(newTokenController.name, "line_FF", error));
     return res.status(500).send(statusMessages.internalerror);
