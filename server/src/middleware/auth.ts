5bf77acd68636ecc73b2f1053b06805cccddf2ec
Çağan Seyrek <115536535+caganseyrek@users.noreply.github.com>
2024-08-06 20:13:21 +0300

    Updated import statement and the function that uses the import

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import { verify } from "jsonwebtoken";
 
 export default (req: Request, _res: Response, next: NextFunction) => {
   const { token } = req.cookies;
   if (token) {
-    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET as string);
-    req.body.user = decoded; // undefined if cannot be encoded
+    const decoded = verify(token, process.env.JWT_ACCESS_SECRET as string);
+    req.body.user = decoded; // undefined if cannot be decoded
   }
   next();
 };
