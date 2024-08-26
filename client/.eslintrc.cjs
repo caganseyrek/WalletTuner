b6e544f221da39899bdf3415852a714fdee89063
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-08-26 15:31:16 +0300

    Added lint-staged and testing frameworks

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -11,5 +11,48 @@ module.exports = {
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "no-alert": "warn",
+    "no-console": ["warn", { allow: ["error"] }],
+    "no-constant-condition": "error",
+    "no-debugger": "error",
+    "no-dupe-keys": "error",
+    "no-duplicate-case": "error",
+    "no-unreachable": "error",
+    "use-isnan": "error",
+    eqeqeq: ["error", "smart"],
+    "no-eval": "error",
+    "no-floating-decimal": "error",
+    "no-new": "error",
+    "no-return-assign": "error",
+    "no-self-compare": "error",
+    "no-undef-init": "error",
+    "no-var": "error",
+    "no-unused-vars": "error",
+    "comma-dangle": ["error", "always-multiline"],
+    "comma-spacing": ["error", { before: false, after: true }],
+    "comma-style": ["error", "last"],
+    "new-parens": "error",
+    "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 1 }],
+    quotes: ["error", "double", { avoidEscape: true }],
   },
+  overrides: [
+    {
+      files: ["src/utils/requester.ts", "src/routes/Dashboard/context/ModalContext.tsx"],
+      rules: {
+        "no-unused-vars": "off",
+      },
+    },
+    {
+      files: ["src/routes/Dashboard/context/ModalContext.tsx"],
+      rules: {
+        "react-refresh/only-export-components": "off",
+      },
+    },
+    {
+      files: ["src/hooks/useOnMountEffect.ts"],
+      rules: {
+        "react-hooks/exhaustive-deps": "off",
+      },
+    },
+  ],
 };
