7387d10bcda789a3f0cb8623e9e901accb1933d7
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-08-26 15:46:38 +0300

    Removed unecessary duplicate semicolons

--- client/src/utils/greeter.ts
+++ client/src/utils/greeter.ts
@@ -7,11 +7,11 @@ function getGreeting(name: string): string {
   if (currentHour >= 5 && currentHour < 12) {
     greeting = i18next.t("greetingMessages.morning");
   } else if (currentHour >= 12 && currentHour < 17) {
-    greeting = i18next.t("greetingMessages.afternoon");;
+    greeting = i18next.t("greetingMessages.afternoon");
   } else if (currentHour >= 17 && currentHour < 21) {
-    greeting = i18next.t("greetingMessages.evening");;
+    greeting = i18next.t("greetingMessages.evening");
   } else {
-    greeting = i18next.t("greetingMessages.night");;
+    greeting = i18next.t("greetingMessages.night");
   }
 
   return `${greeting}, ${name}`;
