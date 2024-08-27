2341209e46e7535953311bda513237b3cc73bb03
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-08-27 11:54:13 +0300

    Added bash script for generating script instead of the previous ts script

--- server/config/secrets.ts
+++
@@ -1,5 +0,0 @@
-import randomBytes from "randombytes";
-
-console.log(randomBytes(64).toString("base64"));
-console.log(randomBytes(64).toString("base64"));
-console.log(randomBytes(64).toString("base64"));
