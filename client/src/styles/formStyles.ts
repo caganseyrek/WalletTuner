f4c246fbad979b47a48a28a6d882584922960ecf
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-08-28 18:30:32 +0300

    Deleted css files and added a ts style file

---
+++ client/src/styles/formStyles.ts
@@ -0,0 +1,42 @@
+import { CSSProperties } from "react";
+
+export const formHeaderStyles: CSSProperties = {
+  paddingTop: "30px",
+  paddingBottom: "15px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  rowGap: "10px",
+};
+
+export const formTitleStyles: CSSProperties = {
+  textAlign: "center",
+  fontWeight: "600",
+  padding: "0px 20px",
+};
+
+export const logoStyles: CSSProperties = {
+  width: "80px",
+  height: "80px",
+  borderRadius: "100%",
+  border: "solid 5px #413d80",
+  boxSizing: "border-box",
+};
+
+export const formPageStyles: CSSProperties = {
+  width: "100wv",
+  height: "100wh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+};
+
+export const formBodyStyles: CSSProperties = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  rowGap: "15px",
+};
