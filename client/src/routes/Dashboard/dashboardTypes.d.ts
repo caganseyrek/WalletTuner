8a37c03d8293470fade577206a77cee61ed88b07
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-08-28 18:31:50 +0300

    Updated dashboard types

--- client/src/routes/Dashboard/dashboardTypes.d.ts
+++ client/src/routes/Dashboard/dashboardTypes.d.ts
@@ -4,15 +4,6 @@ declare interface AccountQueryRequestProps extends EssentialRequestProps {
   accountId?: string;
 }
 
-declare interface AccountQueryResponseProps extends EssentialRequestProps {
-  _id: string;
-  name: string;
-  createdAt: string;
-  balance: number;
-  monthlyIncome: number;
-  monthlyExpense: number;
-}
-
 declare interface AccountCreateRequestProps extends EssentialRequestProps {
   belongsToUser: string;
   name: string;
@@ -24,6 +15,15 @@ declare interface AccountUpdateRequestProps extends EssentialRequestProps {
 
 declare interface AccountDeleteRequestProps extends EssentialRequestProps {
   accountId: string;
+}
+
+declare interface AccountQueryResponseProps {
+  _id: string;
+  name: string;
+  createdAt: string;
+  balance: number;
+  monthlyIncome: number;
+  monthlyExpense: number;
 }
 
 declare interface MessageResponseProps {
