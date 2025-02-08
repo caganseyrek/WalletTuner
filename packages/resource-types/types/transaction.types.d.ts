2dfcadef0cfbcaf32875c1fb3de0e0ea0d5de6ac
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-02-08 22:11:42 +0300

    Moved transaction.d.ts into shared type package

---
+++ packages/resource-types/types/transaction.types.d.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { Globals } from "./global.types";
+
+export namespace Transaction {
+  export interface TransactionProps {
+    _id: mongoose.Types.ObjectId;
+    user_id: mongoose.Types.ObjectId;
+    account_id: mongoose.Types.ObjectId;
+    type: string;
+    amount: number;
+    timestamp: string;
+    note: string;
+  }
+  export interface TransactionPropsWithString
+    extends Omit<Omit<Omit<TransactionProps, "_id">, "user_id">, "account_id"> {
+    _id: string;
+    user_id: string;
+    account_id: string;
+  }
+  export namespace Hook {
+    export type CreateProps = Omit<Omit<TransactionPropsWithString, "_id">, "user_id">;
+    export type UpdateProps = Omit<TransactionPropsWithString, "user_id">;
+    export type DeleteProps = Pick<TransactionPropsWithString, "_id">;
+  }
+  export namespace Controller {
+    export type CreateProps = Globals.UserIdFromCookie & Omit<Omit<TransactionPropsWithString, "_id">, "user_id">;
+    export type UpdateProps = Globals.UserIdFromCookie & Omit<TransactionPropsWithString, "user_id">;
+    export type DeleteProps = Globals.UserIdFromCookie & Pick<TransactionPropsWithString, "_id">;
+  }
+  export namespace Service {
+    export type CreateProps = Globals.IdentifierProps & Omit<Omit<TransactionProps, "_id">, "user_id">;
+    export type UpdateProps = Globals.IdentifierProps & Omit<TransactionProps, "user_id">;
+    export type DeleteProps = Globals.IdentifierProps & Pick<TransactionProps, "_id">;
+  }
+  export namespace Repository {
+    export type CreateProps = Globals.IdentifierProps & Omit<Omit<TransactionProps, "_id">, "user_id">;
+    export type UpdateProps = Globals.IdentifierProps & Omit<TransactionProps, "user_id">;
+    export type DeleteProps = Globals.IdentifierProps & Pick<TransactionProps, "_id">;
+  }
+  export type FindByIdProps = Globals.IdentifierProps & Pick<TransactionProps, "_id">;
+  export type FindByAccountIdProps = Globals.IdentifierProps & Pick<TransactionProps, "account_id">;
+  export type FindByUserIdProps = Globals.IdentifierProps;
+}
