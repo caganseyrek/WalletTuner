be6e915751e9b23f09a9df3bdca7fbfdbe00c5a5
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-02-08 22:11:09 +0300

    Removed unused hook

--- apps/client/hooks/useOnMountEffect.ts
+++
@@ -1,7 +0,0 @@
-import { EffectCallback, useEffect } from "react";
-
-const useOnMountEffect = (effect: EffectCallback): void => {
-  return useEffect(effect, []);
-};
-
-export default useOnMountEffect;
